21c27ae4fa0de0f89a95369362c92b1ce187c7c0
Risti4040 <risti4040@gmail.com>
2026-09-24 06:16:51 +0600

    refactoring database with drizzle schemas

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,20 +1,28 @@
-import "dotenv/config";
 import express from "express";
 
+import { clerkMiddleware } from "@clerk/express";
+import cors from "cors";
+import { ENV } from "./config/env.js";
 import noteRouters from "./routes/noteRoutes.js";
 
 const app = express();
-const port = process.env.PORT || 3000;
-
+app.use(cors({ origin: ENV.FRONTEND_URL }));
 app.use(express.json());
+app.use(clerkMiddleware());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/notes", noteRouters);
 
 app.get("/", async (req, res) => {
-  res.json("oii");
+  res.json({
+    message: "Notepad App",
+    endpoints: {
+      users: "/api/users",
+      notes: "/api/notes",
+    },
+  });
 });
 
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+app.listen(ENV.PORT, () => {
+  console.log(`Server running on http://localhost:${ENV.PORT}`);
 });
